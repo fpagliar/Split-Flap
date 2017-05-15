96f1d679cc891a480e9f07b055f16e8dfeb8900a
Francisco Pagliaricci <fpagliaricci@hotmail.com>
2017-05-14 23:21:03 -0700

    Changed configuration schema
    Now there are 3 configuration files, separating the system parameters,
    the calibration data, and the actual system status

--- src/Configuration.py
+++ src/Configuration.py
@@ -1,63 +1,75 @@
+import collections
 import os.path
 from enum import Enum
 
-def serializeCollection(name, iterable):
-  return serializeValue(name, "|".join([str(elem) for elem in iterable]))
+class ConfigurationFile:
+  _keySeparator = ":"
+  _collectionSeparator = "|"
 
-def serializeValue(name, value):
-  return str(name) + ": " + str(value) + "\n"
+  def __init__(self, fileName):
+    self._fileName = fileName
+    self._mappings = {}
 
-def loadCollection(line, name, func=False):
-  prefix = str(name) + ": "
-  if prefix in line:
-    line = line[len(prefix):]
-  tokens = line.split("|")
-  if func:
-    tokens = [func(elem) for elem in tokens]
-  return tokens
+  def get(self, keyword):
+    return self._mappings[keyword]
 
-def loadValue(line, name, func=False):
-  prefix = str(name) + ": "
-  if prefix in line:
-    line = line[len(prefix):]
-  if func:
-    line = func(line) 
-  return line
+  def set(self, keyword, value):
+    self._mappings[keyword] = value
 
-def defaultSystemConfiguration():
-  config = SystemConfiguration("properties.config")
-  config.setDefaults()
-  config.load()
-  if not config.exists():
-      config.save()
-  return config
+  def cleanup(self):
+    with open(self._filename, "w+") as file:
+      file.truncate(0)
 
-def defaultSystemStatus():
-  status = SystemStatus("system_status.config")
-  status.setDefaults()
-  status.load()
-  return status
+  def save(self):
+    with open(self._filename, "w+") as file:
+      file.truncate(0)
+      for key in self._mappings:
+        file.write(self._serialize(key, self._mappings[key]))
 
-def cleanSystemStatus():
-  return SystemStatus("system_status.config")
+  def _serializeValue(self, key, value):
+    return str(key) + ConfigurationFile._keySeparator + str(value) + "\n"
 
-class Keywords(Enum):
-    TICKS_PER_LETTER = "TICKS_PER_LETTER"
+  def _serialize(self, key, value):
+    if isinstance(value, collections.Iterable):
+      value = ConfigurationFile._collectionSeparator.join([str(elem) for elem in value])
+    return self._serializeValue(key, value)
+
+  def load(self, loaderMap):
+    if os.path.isfile(self._fileName):
+      with open(self._fileName, "r+") as file:
+        for line in file:
+          self._loadLine(line, loaderMap)
+
+  def _loadLine(self, line, loaderMap):
+    strippedLine = line.strip()
+    keySeparator = ConfigurationFile._keySeparator
+    collectionSeparator = ConfigurationFile._collectionSeparator
+    if keySeparator in strippedLine:
+      key = strippedLine.split(keySeparator)[0]
+      value = (strippedLine[len(key + 1):])
+      if collectionSeparator in value:
+        tokens = value.split(collectionSeparator)
+        self._mappings[key] = [loaderMap[key](elem) for elem in tokens]
+      else:
+        self._mappings[key] = loaderMap[key](value)
+
+class _SystemKeywords(Enum):
     NUMBER_OF_MOTORS = "NUMBER_OF_MOTORS"
-    FEED_FILENAME = "FEED_FILENAME"
-    LOG_FILENAME = "LOG_FILENAME"
-    CHARACTERS_ARRAY = "CHARACTERS_ARRAY"
+    USE_DIRECT_CONNECTION = "USE_DIRECT_CONNECTION"
+    SEQUENCE_PINS = "SEQUENCE_PINS"
     DATA_PIN = "DATA_PIN"
     CLOCK_PIN = "CLOCK_PIN"
     SHIFT_PIN = "SHIFT_PIN"
-    SEQUENCE_PINS = "SEQUENCE_PINS"
     DEBUG_MODE = "DEBUG_MODE"
     USE_TEXT_PINS = "USE_TEXT_PINS"
-    USE_DIRECT_CONNECTION = "USE_DIRECT_CONNECTION"
     LOGGER_TAGS = "LOGGER_TAGS"
+    ALPHABET = "ALPHABET"
 
 class SystemConfiguration:
-  def __init__(self, filename):
+  _fileName = "system.config"
+
+  def __init__(self):
+    self._config = ConfigurationFile(SystemConfiguration.filename)
     self.MOTOR_SEQUENCE = [
         [1, 0, 0, 1],
         [1, 0, 0, 0],
@@ -68,141 +80,143 @@ class SystemConfiguration:
         [0, 0, 1, 1],
         [0, 0, 0, 1],
       ][::-1]
-    
-    self._filename = filename
-    self._valueKeywords = [
-      Keywords.TICKS_PER_LETTER,
-      Keywords.NUMBER_OF_MOTORS,
-      Keywords.FEED_FILENAME,
-      Keywords.LOG_FILENAME,
-      Keywords.DATA_PIN,
-      Keywords.CLOCK_PIN,
-      Keywords.SHIFT_PIN,
-      Keywords.DEBUG_MODE,
-      Keywords.USE_TEXT_PINS,
-      Keywords.USE_DIRECT_CONNECTION,
-       ]
-    self._collectionKeywords = [ Keywords.CHARACTERS_ARRAY, Keywords.SEQUENCE_PINS, Keywords.LOGGER_TAGS ]
-    self._loadingFuncs = {
-      Keywords.TICKS_PER_LETTER : int,
-      Keywords.NUMBER_OF_MOTORS : int,
-      Keywords.FEED_FILENAME : str,
-      Keywords.LOG_FILENAME : str,
-      Keywords.DATA_PIN : int,
-      Keywords.CLOCK_PIN : int,
-      Keywords.SHIFT_PIN : int,
-      Keywords.CHARACTERS_ARRAY: str,
-      Keywords.SEQUENCE_PINS: int,
-      Keywords.DEBUG_MODE: bool,
-      Keywords.USE_TEXT_PINS: bool,
-      Keywords.USE_DIRECT_CONNECTION: bool,
-      Keywords.LOGGER_TAGS: str,
-      }
-    
-  def setDefaults(self):
-    self.values = {
-      Keywords.TICKS_PER_LETTER : 3,
-      Keywords.NUMBER_OF_MOTORS : 10,
-      Keywords.FEED_FILENAME : "feed.txt",
-      Keywords.LOG_FILENAME : "log.txt",
-      Keywords.DATA_PIN : 10,
-      Keywords.CLOCK_PIN : 11,
-      Keywords.SHIFT_PIN : 12,
-      Keywords.DEBUG_MODE: True,
-      Keywords.USE_TEXT_PINS: False,
-      Keywords.USE_DIRECT_CONNECTION: False,
-    }
 
-    self.collections = {
-        Keywords.CHARACTERS_ARRAY : ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', ' ', ' ', ' '],
-        Keywords.SEQUENCE_PINS : [19, 5, 22, 17],
-        Keywords.LOGGER_TAGS : [],        
-    }
-  
-  def get(self, key):
-    if key in self.values:
-      return self.values[key]
-    else:
-      return self.collections[key]  
-    
-  def setTicksPerLetter(self, value):
-    self.values[Keywords.TICKS_PER_LETTER] = value
-  
   def save(self):
-    with open(self._filename, "w+") as file:    
-      file.truncate(0)
-      for key in self._valueKeywords:
-        file.write(serializeValue(key, self.values[key]))
-      for key in self._collectionKeywords:
-        file.write(serializeCollection(key, self.collections[key]))
-        
-  def exists(self):
-    return os.path.isfile(self._filename)
+    self._config.save()
 
   def load(self):
-    if os.path.isfile(self._filename):
-      with open(self._filename, "r+") as file:
-        lines = [line.rstrip('\n') for line in file]
-        i = 0
-        for key in self._valueKeywords:
-          self.values[key] = loadValue(lines[i], key, self._loadingFuncs[key])
-          i = i + 1
-        for key in self._collectionKeywords:
-          self.collections[key] = loadCollection(lines[i], key, self._loadingFuncs[key])
-          i = i + 1
+    loaderMap = {
+      _SystemKeywords.NUMBER_OF_MOTORS : int,
+      _SystemKeywords.DATA_PIN : int,
+      _SystemKeywords.CLOCK_PIN : int,
+      _SystemKeywords.SHIFT_PIN : int,
+      _SystemKeywords.SEQUENCE_PINS: int,
+      _SystemKeywords.DEBUG_MODE: bool,
+      _SystemKeywords.USE_TEXT_PINS: bool,
+      _SystemKeywords.USE_DIRECT_CONNECTION: bool,
+      _SystemKeywords.LOGGER_TAGS: str,
+      _SystemKeywords.ALPHABET: str,
+    }
+    self._config.load(loaderMap)
+
+  def _get(self, keyword):
+    return self._config.get(keyword)
+
+  def numberOfMotors(self):
+    return self._get(_SystemKeywords.NUMBER_OF_MOTORS)
+
+  def motorSequence(self):
+    return self.MOTOR_SEQUENCE
+
+  def useDirectConnection(self):
+    return self._get(_SystemKeywords.USE_DIRECT_CONNECTION)
+
+  def sequencePins(self):
+    if self.useDirectConnection():
+      return self._get(_SystemKeywords.SEQUENCE_PINS)
+    else:
+      raise Exception("Use direct connection is set to False, you should be using a shift registry")
+
+  def shiftRegistryPins(self):
+    if self.useDirectConnection():
+      raise Exception("Use direct connection is set to True, so you should be using the sequence pins")
+    else:
+      return (self._get(_SystemKeywords.DATA_PIN), self._get(_SystemKeywords.CLOCK_PIN), self._get(_SystemKeywords.SHIFT_PIN))
+
+  def isDebugMode(self):
+    return self._get(_SystemKeywords.DEBUG_MODE)
+
+  def shouldUseTextPins(self):
+    if self.isDebugMode():
+      return self._get(_SystemKeywords.USE_TEXT_PINS)
+    else:
+      return False
+
+  def loggerTags(self):
+    if self.isDebugMode():
+      return self._get(_SystemKeywords.LOGGER_TAGS)
+    else:
+      return []
+
+  def alphabet(self):
+    return self._get(_SystemKeywords.ALPHABET)
+
+class MotorConfigurationFile:
+  _separator = ">"
+
+  def __init__(self, fileName, numberOfMotors):
+    self._config = ConfigurationFile(fileName)
+    self._numberOfMotors = numberOfMotors
+
+  def _getKey(self, motorId, field):
+    return str(motorId) + MotorConfigurationFile._separator + field
+
+  def save(self):
+    self._config.save()
+
+  def cleanup(self):
+    self._config.cleanup()
+
+  def load(self, keyFunctionMap):
+    loaderMap = { }
+    for i in range(1, self._numberOfMotors):
+      for key in keyFunctionMap:
+        loaderMap[self._getKey(i, key)] = keyFunctionMap[key]
+    self._config.load(loaderMap)
+
+  def get(self, motorId, key):
+    return self._config.get(self._getKey(motorId, key))
+
+  def set(self, motorId, key, value):
+    if motorId not in range(1, self._numberOfMotors):
+      raise Exception("Invalid motor id: " + str(motorId))
+    self._config.set(self._getKey(motorId, key), value)
 
 class SystemStatus:
-  def __init__(self, filename):
-    self._filename = filename
-    self._details = {}
-    self._ticksKey = "TICKS"
-    self._indexKey = "LETTER_INDEX"
-    self._sequenceKey = "SEQUENCE_INDEX"
-    self._keySeparator = ">"
-  
-  def set(self, motorId, currentTicks, currentLetterIndex, sequenceIndex):
-    self._details[motorId] = { self._ticksKey: currentTicks, self._indexKey: currentLetterIndex, self._sequenceKey: sequenceIndex }
+  _fileName = "system.status"
+  _ticksKey = "TICKS"
+  _sequenceKey = "SEQUENCE_INDEX"
 
-  def setDefaults(self):
-      for index in range(10):
-        self.set(index, 0, 0, 0)
-  
+  def __init__(self, numberOfMotors):
+    self._config = MotorConfigurationFile(SystemStatus._fileName, numberOfMotors)
+
+  def set(self, motorId, currentTicks, sequenceIndex):
+    self._config.set(SystemStatus._ticksKey, currentTicks)
+    self._config.set(SystemStatus._sequenceKey, sequenceIndex)
+
+  def ticks(self, motorId):
+    return self._config.get(motorId, SystemStatus._ticksKey)
+
+  def sequence(self, motorId):
+    return self._config.get(motorId, SystemStatus._sequenceKey)
+
   def save(self):
-    with open(self._filename, "w+") as file:
-      for key, value in self._details.items():
-        file.write(serializeValue(str(key) + self._keySeparator + self._ticksKey , value[self._ticksKey]))
-        file.write(serializeValue(str(key) + self._keySeparator + self._indexKey , value[self._indexKey]))
-        file.write(serializeValue(str(key) + self._keySeparator + self._sequenceKey , value[self._sequenceKey]))
-          
+    self._config.save()
+
   def cleanup(self):
-    with open(self._filename, "w+") as file:
-      file.truncate(0)
+    self._config.cleanup()
 
-  def exists(self):
-    return os.path.isfile(self._filename)
-      
   def load(self):
-    if os.path.isfile(self._filename):
-      with open(self._filename, "r+") as file:
-        for line in file:
-          motorId, rest = line.split(self._keySeparator)
-          motorId = int(motorId)
-          key, value = rest.split(":")
-          if motorId not in self._details:
-            self._details[motorId] = {}
-          self._details[motorId][key] = int(value)
-  
-  def getCurrentTicks(self, motorId):
-    return self._getDetail(motorId, self._ticksKey)
+    self._config.load({ SystemStatus._sequenceKey: int, SystemStatus._ticksKey: int})
 
-  def getCurrentLetterIndex(self, motorId):
-    return self._getDetail(motorId, self._indexKey)
+class SystemCalibration:
+  _fileName = "system.calibration"
+  _key = "LETTER_CHANGES"
 
-  def getSequenceIndex(self, motorId):
-    return self._getDetail(motorId, self._sequenceKey)
+  def __init__(self, numberOfMotors):
+    self._config = MotorConfigurationFile(SystemStatus._fileName, numberOfMotors)
 
-  def _getDetail(self, motorId, key):
-    if motorId in self._details and key in self._details[motorId]:
-      return self._details[motorId][key]
-    else:
-      return 0
+  def set(self, motorId, ticksConfiguration):
+    self._config.set(SystemCalibration._key, ticksConfiguration)
+
+  def ticksConfiguration(self, motorId):
+    return self._config.get(motorId, SystemCalibration._key)
+
+  def save(self):
+    self._config.save()
+
+  def cleanup(self):
+    self._config.cleanup()
+
+  def load(self):
+    self._config.load({ SystemCalibration._key : int})
