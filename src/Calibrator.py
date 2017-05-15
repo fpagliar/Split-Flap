e0a41185b3a98929ee63274f661e8ff6524a9a0c
Francisco Pagliaricci <fpagliaricci@hotmail.com>
2017-05-14 23:52:22 -0700

    Calibrator ticks_per_letter for every letter
    Changed the calibrator, removed the divide and conquer strategy, and
    went back to the original one, but now it supports this idea of not a
    fixed value for ticks_per_letter, but a specific value for each letter,
    in each character

--- src/Calibrator.py
+++ src/Calibrator.py
@@ -1,88 +1,71 @@
-from Configuration import defaultSystemConfiguration, cleanSystemStatus
 from DisplayFactory import DisplayFactory
-from Configuration import Keywords
 from Logger import log
 import Utils
 import time
+from Configuration import SystemConfiguration, SystemCalibration, SystemStatus
 
-# This class takes care of the calibration of each character. Calibration consists on figuring out how many steps of the sequence 
-# are needed to change a letter, and also on setting the initial positions for each letter.
+# This class takes care of the calibration of each character.
+# calibrateInitialPosition will help us set the start position of the system. This is the way the program will be able to
+# know which letter it is showing at the moment, what step is the motor sequence in, and how many steps till the next letter shows.
+# Once you calibrate the initial position, it will be saved to a file and there is not need to set it again unless something bad happens
+# The other part is calibrateTicksPerLetter, and this will save which is the composition of the characters and the physical system
+# and will be saved and shouldn't be run again unless you change/replace a character.
 class Calibrator:
   def __init__(self, pinBuilder):
     self._pinBuilder = pinBuilder
-    self._config = defaultSystemConfiguration()
+    self._config = SystemConfiguration()
 
   def infiniteRun(self):
     controller = _CalibratorCharacter(self._pinBuilder, 1)
     while True:
       controller.tick()
       time.sleep(0.001)
-        
-  def calibrateTicksPerLetter(self):
-      self.divideAndConquerTicksPerLetter()
-    
-  def divideAndConquerTicksPerLetter(self):
-    controller = _CalibratorCharacter(self._pinBuilder, 1)
-    print("Please take a look at the first character, we will need to validate when the letter is changed for setup")
-    
-    ticks = self.divideAndConquer(controller, 50, 100)
-    
-    self._config.setTicksPerLetter(ticks)
-    self._config.save()
 
-  # If value is 76, (50, 100) -> (75, 100) -> (75, 87) -> ....
-  def divideAndConquer(self, controller, lowValue, highValue):
-    if lowValue == highValue:
-        return lowValue
-    
-    log(Calibrator.__name__, "Calibrator", "Testing range (" + str(lowValue) + ", " + str(highValue) + ")")
-    for _ in range(highValue * 2):
-      controller.tick(0)
-    highValueTurnedLetter = Utils.askForConfirmation("Did it change two letters?")
-    for _ in range(lowValue * 2):
-      controller.tick(0)
-    lowValueTurnedLetter = Utils.askForConfirmation("Did it change two letters?")
-    
-    # To avoid getting stuck in loops due to int casting
-    if highValue - lowValue == 1:
-      if highValueTurnedLetter and lowValueTurnedLetter:
-        return lowValue
-      elif highValueTurnedLetter and not lowValueTurnedLetter:
-        return highValue
-    
-    if highValueTurnedLetter and lowValueTurnedLetter:
-      return self.divideAndConquer(controller, int(lowValue/2), lowValue)
-    elif highValueTurnedLetter and not lowValueTurnedLetter:
-      return self.divideAndConquer(controller, int(lowValue + ((highValue - lowValue)/2)), highValue)
-    elif not highValueTurnedLetter and not lowValueTurnedLetter:
-      return self.divideAndConquer(controller, int(lowValue/2), lowValue)
-    else:
-      raise Exception("Low value " + str(lowValue) + "turned letter, but high one " + str(highValue) + " didn't")
-        
-  def calibrateInitialPosition(self):
-    numberOfMotors = self._config.get(Keywords.NUMBER_OF_MOTORS)
+  def calibrateTicksPerLetter(self):
+    self.calibrateInitialPosition()
+    numberOfMotors = self._config.numberOfMotors()
     controller = _CalibratorCharacter(self._pinBuilder, numberOfMotors)
-    systemStatus = cleanSystemStatus()
+    calibration = SystemCalibration()
+    alphabet = self._config.alphabet()
+
+    for i in range (1, numberOfMotors + 1):
+      print("Now calibrating the character " + str(i))
+      ticksConfiguration = []
+      ticks = 0
+      for letter in alphabet[:1]:  # We start with A showing, so the next letter should be the first target
+        while not Utils.askForConfirmation("Is it showing character " + letter + "?", 0.5):
+          controller.tick(i - 1)
+          ticks = ticks + 1
+        ticksConfiguration.append(ticks)
+      calibration.set(i, ticksConfiguration)
+    print("Great, calibration ended")
+    calibration.save()
+
+  def calibrateInitialPosition(self):
+    numberOfMotors = self._config.numberOfMotors()
+    controller = _CalibratorCharacter(self._pinBuilder, numberOfMotors)
+    systemStatus = SystemStatus()
     systemStatus.cleanup()
+    target = self._config.alphabet()[0]
+
     for i in range (1, numberOfMotors + 1):
       print("Now configuring the character " + str(i))
-      # This will help us take the ticks variable to zero (first appearance of the letter = 0)
-      # Plus it will also configure our sequence to execute correctly (if the motor is waiting for 
-      # sequence 5 and we start from zero, it will ignore our calls 0-4, and start turning from 5 onwards.
-      while not Utils.askForConfirmation("Is it showing letter B now?", True):
-        controller.tick(i-1)
-      systemStatus.set(i, 0, self._config.get(Keywords.CHARACTERS_ARRAY).index('B'), controller.getSequence().currentIndex())
+      while not Utils.askForConfirmation("Is it close to " + target + "?", 0.05):
+        controller.tick(i - 1)
+      while not Utils.askForConfirmation("Is it showing letter " + target + "?", 0.5):
+        controller.tick(i - 1)
+      systemStatus.set(i, 0, controller.getSequence().currentIndex())
     systemStatus.save()
     print("Great, now the split-flap is correctly configured")
-  
+
 class _CalibratorCharacter:
   def __init__(self, pinBuilder, quantity):
-    factory = DisplayFactory(pinBuilder, defaultSystemConfiguration(), cleanSystemStatus())
+    factory = DisplayFactory(pinBuilder, SystemConfiguration(), SystemStatus())
     self._sequences, self._connection = factory.buildCharacterCalibrator(quantity)
-    
+
   def getSequences(self):
     return self._sequences
-    
+
   def tick(self, index):
     self._sequences[index].next()
     self._connection.publish()
