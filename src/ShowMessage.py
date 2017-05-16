98c68a2d2866179f07b37fde1ad7ecfa106f663b
Francisco Pagliaricci <fpagliaricci@hotmail.com>
2017-05-15 22:04:33 -0700

    Remoing extra runnables
    Since we have a generic main class now, this runnables are dead

--- src/ShowMessage.py
+++
@@ -1,7 +0,0 @@
-from DisplayFactory import DisplayFactory
-import Pin
-from Configuration import defaultSystemConfiguration, defaultSystemStatus
-
-if __name__ == "__main__":
-  display = DisplayFactory(Pin.GetTextPin, defaultSystemConfiguration(), defaultSystemStatus()).build()
-  display.show("ABCDEFGH")
