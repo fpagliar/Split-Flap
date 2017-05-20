30569f25a64db2b27bb8595322d82998b5dd9d13
Francisco Pagliaricci <fpagliaricci@hotmail.com>
2017-05-19 19:42:52 -0700

    Shift Registry executable option
    + Added a runnable option to just test the shift registry directly, and
    avoid interacting with the other components

--- src/DisplayFactory.py
+++ src/DisplayFactory.py
@@ -1,9 +1,8 @@
 from MotorSequence import MotorSequence
 from Display import Display
-from Configuration import Keywords
 from Character import Character
 from Timer import Timer
-from Connection import buildConnection, SequencePublisher
+from Connection import buildConnection, SequencePublisher, _ShiftRegistry, _ShiftRegistryController
 from Logger import log
 
 # This class is a factory to produce the different types of displays based on the configuration provided.
@@ -12,7 +11,7 @@ class DisplayFactory:
     self._pinBuilder = pinBuilder
     self._config = configuration
     self._systemStatus = systemStatus
-    
+
   def buildCharacterCalibrator(self, quantity):
     sequences = [self._createMotorSequence(i + 1) for i in range(quantity)]
     connection = buildConnection(self._config, self._pinBuilder, quantity, Timer())
@@ -24,19 +23,24 @@ class DisplayFactory:
     publisher = SequencePublisher([sequence], connection)
     character = Character(1, sequence, self._config, self._systemStatus)
     return Display([character], publisher)
-    
+
+  def buildShiftRegistry(self, length):
+    data, clock, shift = self._config.shiftRegistryPins()
+    controller = _ShiftRegistryController(self._pinBuilder(data), self._pinBuilder(clock), self._pinBuilder(shift), Timer())
+    return _ShiftRegistry(controller, length)
+
   def _getPin(self, keyword):
     return self._pinBuilder(self._config.get(keyword))
-  
+
   def build(self):
-    numberOfMotors = self._config.get(Keywords.NUMBER_OF_MOTORS)
+    numberOfMotors = self._config.numberOfMotors()
     sequences = [self._createMotorSequence(i + 1) for i in range(numberOfMotors)]
     connection = buildConnection(self._config, self._pinBuilder, numberOfMotors, Timer())
     characters = [Character(motorId + 1, sequences[motorId], self._config, self._systemStatus) for motorId in range(numberOfMotors)]
     return Display(characters, SequencePublisher(sequences, connection))
-  
+
   def _createMotorSequence(self, motorId):
-    return MotorSequence(self._config.MOTOR_SEQUENCE, self._systemStatus.getSequenceIndex(motorId))
-  
+    return MotorSequence(motorId, self._config.MOTOR_SEQUENCE, self._systemStatus.sequence(motorId))
+
   def _createPinsFromIds(self, ids):
     return [self._pinBuilder(ids[i]) for i in range(len(ids))]
--- src/Main.py
+++ src/Main.py
@@ -3,34 +3,49 @@ import Pin
 import time
 from Calibrator import Calibrator
 from DisplayFactory import DisplayFactory
-from Configuration import defaultSystemConfiguration, defaultSystemStatus, Keywords
+from Configuration import SystemConfiguration, SystemStatus
+import re
 
 option = sys.argv[1]
+config = SystemConfiguration()
 
 if option == "--position":
-    calibrator = Calibrator(Pin.GetPin)
+    calibrator = Calibrator(Pin.GetPin, config)
     calibrator.calibrateInitialPosition()
 elif option == "--ticks":
-    calibrator = Calibrator(Pin.GetPin)
+    calibrator = Calibrator(Pin.GetPin, config)
     calibrator.calibrateTicksPerLetter()
 elif option == "--message":
-    display = DisplayFactory(Pin.GetPin, defaultSystemConfiguration(), defaultSystemStatus()).build()
+    display = DisplayFactory(Pin.GetPin, config, SystemStatus(config.numberOfMotors())).build()
     display.show(sys.argv[2])
 elif option == "--show":
-    display = DisplayFactory(Pin.GetPin, defaultSystemConfiguration(), defaultSystemStatus()).buildCharacterTester()
+    display = DisplayFactory(Pin.GetPin, config, SystemStatus(config.numberOfMotors())).buildCharacterTester()
     while True:
         display.show(input("What do you want to show next?"))
 elif option == "--character":
-    config = defaultSystemConfiguration()
-    display = DisplayFactory(Pin.GetPin, config, defaultSystemStatus()).buildCharacterTester()
-    for char in config.get(Keywords.CHARACTERS_ARRAY):
+    display = DisplayFactory(Pin.GetPin, config, SystemStatus(config.numberOfMotors())).buildCharacterTester()
+    for char in config.alphabet():
         display.show(char)
         time.sleep(5)
 elif option == "--infinite":
     calibrator = Calibrator(Pin.GetPin)
     calibrator.infiniteRun()
-elif option == "--config":
-    defaultSystemConfiguration().save()
+elif option == "--shift-registry":
+    length = 0
+    while length <= 0:
+      answer = input("Enter the desired length of the shift registry: ")
+      if answer.isdigit():
+        length = int(answer)
+    registry = DisplayFactory(Pin.GetPin, config, SystemStatus(length)).buildShiftRegistry(length)
+    regex = re.compile("[01]{" + str(length) + "}")
+    while True:
+      pattern = input("What do you want to show?")
+      if regex.match(pattern) is None:
+        print("Invalid input: " + pattern)
+      else:
+        binary = [x == "1" for x in pattern]
+        registry.set(binary)
+
 else:
     raise Exception("Invalid option")
 
